27b12a034f0c74949e4be714c5d678150639037a
maru_feb <fabio.maruca03@gmail.com>
2023-08-05 17:48:00 +0200

    Refactored tests

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -1,91 +1,68 @@
 package test;
 
-
 import com.scai.gestorefile_v03.models.Document;
 import com.scai.gestorefile_v03.services.DocumentService;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
-
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
-    private DocumentService documentService = new DocumentService();
+    private final DocumentService documentService = new DocumentService();
 
-    /*
     @Test
-    public void mathTest() {
-        int result = Math.addExact(4,4);
-        assertEquals(8, result);
-    }
-    */
+    @SneakyThrows
+    void shouldDeleteFile() {
+        // GIVEN
+        Path prova = Files.createTempFile("prova", ".txt");
 
+        // WHEN - THEN
+        assertTrue(documentService.deleteByPath(prova));
 
+        assertFalse(prova.toFile().exists());
 
-    @ParameterizedTest
-    @MethodSource("addFileParameters")
-    public void addFileTest(int id, String tag, Path path) {
-        Document document = documentService.addFile(id, tag, path);
-        assertTrue(document.getFile().exists());
-
-    }
-    public static Stream<Arguments> addFileParameters() {
-        Path path = Path.of("C:/Users/lucam/OneDrive/Desktop/GestoreFile/GestoreFile_V03/src/main/resources/database/prova02.txt");
-        return Stream.of(Arguments.of(1,"tag1",path));
-
+        Files.deleteIfExists(prova);
     }
 
+    @Test
+    @SneakyThrows
+    void shouldDeleteDocument() {
+        // GIVEN
+        Path prova = Files.createTempFile("prova", ".txt");
 
+        Document document = new Document(1, "tag1", prova);
 
-
-    @ParameterizedTest
-    @MethodSource("deleteByPathParameters")
-    public void deleteByPathTest(Path path) {
-        documentService.deleteByPath(path);
-        assertTrue(documentService.deleteByPath(path));
-    }
-    public static Stream<Arguments> deleteByPathParameters() {
-        Path path = Path.of("C:/Users/lucam/OneDrive/Desktop/GestoreFile/GestoreFile_V03/src/main/resources/database/prova02.txt");
-        return Stream.of(Arguments.of(path));
-    }
-    @ParameterizedTest
-    @MethodSource("deleteParameters")
-    public void deleteTest(Document document) {
-        documentService.delete(document);
+        // WHEN - THEN
         assertTrue(documentService.delete(document));
 
-    }
-    public static Stream<Arguments> deleteParameters() {
-        Path path = Path.of("C:/Users/lucam/OneDrive/Desktop/GestoreFile/GestoreFile_V03/src/main/resources/database/prova02.txt");
-        return Stream.of(Arguments.of(new Document(1,"tag1", path)));
-    }
+        assertFalse(prova.toFile().exists());
 
-
-
-
-    @ParameterizedTest
-    @MethodSource("moveParameters")
-    public void moveTest(Document doc, Path path) {
-        documentService.move(doc, path);
-        assertTrue(documentService.move(doc, path));
+        Files.deleteIfExists(prova);
     }
 
-    public static Stream<Arguments> moveParameters() {
-        Path pathSource = Path.of("C:/Users/lucam/OneDrive/Desktop/GestoreFile/GestoreFile_V03/src/main/resources/database/provaMove01.txt");
-        Path pathDestination = Path.of("C:/Users/lucam/OneDrive/Desktop/GestoreFile/GestoreFile_V03/src/main/resources/databaseMove/provaMove02.txt");
-        return Stream.of(Arguments.of(new Document(2, "tag2", pathSource), pathDestination));
+    @Test
+    @SneakyThrows
+    void shouldAddFile() {
+        // GIVEN
+        Path prova = Files.createTempFile("prova", ".txt");
+        Files.delete(prova);
 
+        assertFalse(prova.toFile().exists());
+
+        // WHEN
+        Document test = documentService.addFile(0, "test", prova);
+
+
+        // THEN
+        assertEquals(prova, test.getPath());
+        assertTrue(test.getFile().exists());
+
+        Files.deleteIfExists(prova);
     }
-
-
 
 
 }
